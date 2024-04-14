478aaf441942ad58ad77bffc8c38a0a61614dcc6
SimretN <134206815+SimretN@users.noreply.github.com>
2024-04-14 20:55:48 +0200

    Footer and header created

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,13 +1,31 @@
 
 import "./App.css";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import { useState } from "react";
+
 
 function App() {
+  // const  [search, setSearch] = useState({""})
+
+const searchClicked = () => {
+  console.log(searchClicked, "simret clicked");
+}
 
   return (
     <>
+    <Header/>
       <div>
-        <h1 className="text-3xl font-bold underline">Weather App</h1>
+        <input type="text" placeholder="search coountries" className="text-xl font-bold border"/>
+      <button onClick={searchClicked} className="bg-violet-400 text-white m-4 px-4 py-2 rounded-lg m-1 shadow-lg">Search</button>
       </div>
+      {/* citylist */}
+      <p>Stockholm</p>
+      {/* degreeCentigrade */}
+      <p>30 C°</p>
+      {/* feeling */}
+      <p>Sunny</p>
+      <Footer/>
     </>
   );
 }
