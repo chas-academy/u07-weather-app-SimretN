0af39bf901de057520ebe52443726d53c0da9341
SimretN <134206815+SimretN@users.noreply.github.com>
2024-04-22 10:00:17 +0200

    some functionality has been changed

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,31 +1,37 @@
-
 import "./App.css";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
 import { useState } from "react";
 
-
 function App() {
   // const  [search, setSearch] = useState({""})
 
-const searchClicked = () => {
-  console.log(searchClicked, "simret clicked");
-}
+  const searchClicked = () => {
+    console.log(searchClicked, "simret clicked");
+  };
 
   return (
     <>
-    <Header/>
-      <div>
-        <input type="text" placeholder="search coountries" className="text-xl font-bold border"/>
-      <button onClick={searchClicked} className="bg-violet-400 text-white m-4 px-4 py-2 rounded-lg m-1 shadow-lg">Search</button>
+      <div className="bg-black py-5  ">
+        <input
+          type="text"
+          placeholder="search coountries"
+          className="border-solid border-2 rounded-lg m-2 p-1"
+        />
+        <button
+          onClick={searchClicked}
+          className="bg-violet-400 text-white m-4 px-4 py-2 rounded-lg m-1 shadow-lg"
+        >
+          Search
+        </button>
+
+        {/* citylist */}
+        <p className="bg-violet-400  ">Stockholm</p>
+        {/* degreeCentigrade */}
+        <p className="bg-red-400  text-center">30 C°</p>
+        {/* feeling */}
+        <p className="bg-green-400 ">Sunny</p>
       </div>
-      {/* citylist */}
-      <p>Stockholm</p>
-      {/* degreeCentigrade */}
-      <p>30 C°</p>
-      {/* feeling */}
-      <p>Sunny</p>
-      <Footer/>
     </>
   );
 }
