4b0c8495096ceae8a12e6ef81ccab8fbbd2d367e
SimretN <134206815+SimretN@users.noreply.github.com>
2024-05-02 14:06:29 +0200

    created footer component

---
+++ weatherapp-js/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <p className="hover:text-blue-500 text-white">
+        Developed By <a href="https://github.com/SimretN">Simret Negash</a>
+      </p>
+    </>
+  );
+};
+
+export default Footer;
